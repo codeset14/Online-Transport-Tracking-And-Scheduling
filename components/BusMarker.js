2a2ca99b861b59162de277e2abc6aca0d7e5b5bb
ankit8801 <ankitjadhav7865@gamil.com>
2025-09-16 17:01:47 +0530

    Delete all files in main branch

--- components/BusMarker.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Marker } from 'react-native-maps';
-import busIcon from '../assets/bus-blue.png'; // add bus icon
-
-export default function BusMarker({ coordinate }) {
-  return <Marker coordinate={coordinate} image={busIcon} />;
-}
--- constants/colors.js
+++
@@ -1,9 +0,0 @@
-export const COLORS = {
-  primary: '#1E90FF',
-  secondary: '#FF6347',
-  user: "#0D6EFD",
-  driver: '#FFA500',
-  admin: '#9C27B0',
-  background: '#F5F5F5',
-  white: '#FFFFFF',
-};
